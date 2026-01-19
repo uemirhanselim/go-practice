abd44f35935415ae89946efc0f7da62f4a3f4b20
uemirhanselim <uemirhanselim@gmail.com>
2026-01-19 09:43:46 +0300

    if condition

--- third.go
+++ third.go
@@ -6,5 +6,13 @@ func main() {
 	//users := map[string]int{}
 	users := make(map[string]int)
 	users["esu"] = 23
-	fmt.Printf("this is: %v\n", users)
+
+	age, ok := users["esu"]
+
+	if !ok {
+		fmt.Println("Bar does not exist")
+	} else {
+		fmt.Printf("this exist: %d\n", age)
+	}
+
 }
