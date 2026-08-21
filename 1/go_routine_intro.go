6341f641b9fddd15aac6470b39d21ac3cb98220b
uemirhanselim <uemirhanselim@gmail.com>
2026-08-21 23:48:40 +0300

    sync

--- 1/go_routine_intro.go
+++ 1/go_routine_intro.go
@@ -2,34 +2,40 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func delayedHello() {
-	fmt.Println("Starting delayedHello")
-	time.Sleep(2 * time.Second)
-	fmt.Println("Hello, World!")
+type Server struct {
+	quitch chan struct{}
+	msgch  chan string
+}
+
+func newServer() *Server {
+	return &Server{
+		quitch: make(chan struct{}),
+		msgch:  make(chan string),
+	}
+}
+
+func (s *Server) start() {
+	fmt.Println("Server is starting...")
+}
+
+func (s *Server) loop() {
+	for {
+		select {
+		case <-s.quitch:
+		case msg := <-s.msgch:
+			s.handleMessage(msg)
+		default:
+		}
+
+	}
+}
+
+func (s *Server) handleMessage(msg string) {
+	fmt.Println("Received message:", msg)
 }
 
 func main() {
-	msgch := make(chan string, 128)
 
-	msgch <- "A"
-	msgch <- "B"
-	msgch <- "C"
-	close(msgch)
-
-	for {
-		msg, ok := <-msgch
-
-		if !ok {
-			break
-		}
-
-		fmt.Println(msg)
-	}
-
-	// for msg := range msgch {
-	// 	fmt.Println(msg)
-	// }
 }
