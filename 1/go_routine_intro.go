c6f4ec69d39417fcc40cf62f332b2b6040760c4d
uemirhanselim <uemirhanselim@gmail.com>
2026-08-23 23:40:11 +0300

    Özet: WaitGroup = bekle; Mutex = sırayla gir; Atomic = basit sayılar için hızlı koruma. Hepsi aynı problemi farklı açıdan çözer.

--- 1/go_routine_intro.go
+++ 1/go_routine_intro.go
@@ -1,59 +1,65 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "sync/atomic"
 
-type Server struct {
-	quitch chan struct{}
-	msgch  chan string
+type State struct {
+	//mu    sync.Mutex
+	count int32
 }
 
-func newServer() *Server {
-	return &Server{
-		quitch: make(chan struct{}),
-		msgch:  make(chan string, 128),
-	}
-}
-
-func (s *Server) sendMessage(msg string) {
-	s.msgch <- msg
-}
-
-func (s *Server) start() {
-	fmt.Println("Server is starting...")
-	s.loop()
-}
-
-func (s *Server) stop() {
-	close(s.quitch)
-}
-
-func (s *Server) loop() {
-mainloop:
-	for {
-		select {
-		case <-s.quitch:
-			fmt.Println("quit")
-			break mainloop
-		case msg := <-s.msgch:
-			s.handleMessage(msg)
-		}
-
-	}
-	fmt.Println("quitting gracefully")
-}
-
-func (s *Server) handleMessage(msg string) {
-	fmt.Println("Received message:", msg)
+func (s *State) setState(i int) {
+	atomic.AddInt32(&s.count, int32(i))
+	//s.mu.Lock()
+	//defer s.mu.Unlock()
+	//s.count = i
 }
 
 func main() {
-	s := newServer()
-	go func() {
-		time.Sleep(5 * time.Second)
-		s.stop()
-	}()
-	s.start()
+
 }
+
+// Keeping Server alive
+
+// type Server struct {
+// 	quitch chan struct{}
+// 	msgch  chan string
+// }
+
+// func newServer() *Server {
+// 	return &Server{
+// 		quitch: make(chan struct{}),
+// 		msgch:  make(chan string, 128),
+// 	}
+// }
+
+// func (s *Server) sendMessage(msg string) {
+// 	s.msgch <- msg
+// }
+
+// func (s *Server) start() {
+// 	fmt.Println("Server is starting...")
+// 	s.loop()
+// }
+
+// func (s *Server) stop() {
+// 	close(s.quitch)
+// }
+
+// func (s *Server) loop() {
+// mainloop:
+// 	for {
+// 		select {
+// 		case <-s.quitch:
+// 			fmt.Println("quit")
+// 			break mainloop
+// 		case msg := <-s.msgch:
+// 			s.handleMessage(msg)
+// 		}
+
+// 	}
+// 	fmt.Println("quitting gracefully")
+// }
+
+// func (s *Server) handleMessage(msg string) {
+// 	fmt.Println("Received message:", msg)
+// }
---
+++ 1/go_routine_intro_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func TestServer(t *testing.T) {
+	state := &State{}
+	wg := sync.WaitGroup{}
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(i int) {
+			state.setState(i + 1)
+			wg.Done()
+		}(i)
+
+	}
+	wg.Wait()
+	fmt.Println(state.count)
+}
